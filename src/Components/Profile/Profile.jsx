2e12200d41450a2c8c25fec24349cd71e7b268ef
Ruslan Garmash <garmash.rb@gmal.com>
2024-06-28 22:20:30 +0300

    udate_завдання_1

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,23 +1,10 @@
 import s from './Profile.module.css'
 
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
-
 const Profile = ({ username, tag, location, image  }) => {
 
     
   return (
-    <div className={s.wrapper}>
+    <div className={s.userCard}>
       
       <div className={s.userDate}>
         <div className={s.imageWrapper}>
