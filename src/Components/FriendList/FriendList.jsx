ea3b9e079e0892d3ea5611636b13534a91a20e30
Ruslan Garmash <garmash.rb@gmal.com>
2024-06-30 18:20:25 +0300

    add css friend

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,8 +1,9 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
+import s from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={s.friendList}>
       {friends.map((friend) => (
         <li key={friend.id}>
           <FriendListItem friend={friend} />
--- src/Components/FriendListItem/FriendListItem.jsx
+++ src/Components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,14 @@
-const FriendListItem = ({ friend }) => {
+import s from "./FriendListItem.module.css";
+import clsx from "clsx";
+
+const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
   return (
-    <div>
-      <img src={friend.avatar} alt="Avatar" width="40" />
-      <p>{friend.name}</p>
-      <p>onlin</p>
+    <div className={s.friendWrapper}>
+      <img src={avatar} alt="Avatar" width="50" />
+      <p>{name}</p>
+      <p className={clsx(s.status, isOnline === true ? s.online : s.offline)}>
+        {isOnline === true ? "Online" : "Offline"}
+      </p>
     </div>
   );
 };
